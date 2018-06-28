577e960c361af6e9ab19bc317f7e5c5b401d2ce9
Maksim Zhvirblia <maksim.zhvirblia7@gmail.com>
2018-06-28 09:32:15 +0300

    add data provider with excel file

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,7 +1,17 @@
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import javax.imageio.ImageIO;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Configuration {
@@ -11,8 +21,25 @@ public class Configuration {
     public void runAndConfigDriver() {
         ChromeDriverManager.getInstance().setup();
         driver = new ChromeDriver();
-//        driver.manage().window().maximize();
+        driver.manage().window().maximize();
+//        driver.manage().window().fullscreen();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+    public void takeScreenshot() throws IOException {
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("src/test/resources/screenshots/screen.png"));
+    }
+
+    public File captureElement(WebElement element) throws Exception {
+        File screen = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        BufferedImage img = ImageIO.read(screen);
+        int width = element.getSize().getWidth();
+        int height = element.getSize().getHeight();
+        Rectangle rect = new Rectangle(width, height);
+        Point p = element.getLocation();
+        BufferedImage dest = img.getSubimage(p.getX(), p.getY(), rect.width, rect.height);
+        ImageIO.write(dest, "png", screen);
+        return screen;
+    }
 }
--- src/test/java/Authorization.java
+++ src/test/java/Authorization.java
@@ -1,10 +1,21 @@
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -13,11 +24,6 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Authorization extends Configuration {
-
-//    WebDriver driver;
-//    public Authorization(WebDriver driver) {
-//        super(driver);
-//    }
 
     private page.Homepage Homepage;
 
@@ -29,8 +35,9 @@ public class Authorization extends Configuration {
     }
 
     @Test
-    public void loginTest() {
+    public void loginTest() throws IOException {
         Homepage.logining("Autotest", "123456789");
+        takeScreenshot();
         Assert.assertEquals(Homepage.loginedName(), "Autotest");
         Homepage.logoutButton();
     }
@@ -45,7 +52,9 @@ public class Authorization extends Configuration {
 //    }
 
     @DataProvider
-    public Object[][] getData() throws FileNotFoundException {
+    public Object[][] getJsonData() throws FileNotFoundException {
+
+        //from json
         JsonElement jsonData = new JsonParser().parse(new FileReader("src/test/resources/test.json"));
         JsonElement dataSet = jsonData.getAsJsonObject().get("dataSet");
         List<TestData> testData = new Gson().fromJson(dataSet, new TypeToken<List<TestData>>() {
@@ -58,14 +67,48 @@ public class Authorization extends Configuration {
         return returnValue;
     }
 
-    @Test(dataProvider = "getData")
+    @Test(dataProvider = "getJsonData")
     public void negativeloginTest(TestData data) {
         Homepage.logining(data.getLogin(), data.getPass());
         Assert.assertEquals(Homepage.loginFailed(), "Ошибка авторизации");
     }
 
+    //from excel
+    @DataProvider
+    public Object[][] getExcelData() {
+
+        Integer start = 0;
+        Object[][] returnValue = new Object[5][2];
+        try {
+
+            FileInputStream fis = new FileInputStream(new File("src/test/resources/excel.xlsx"));
+
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+            XSSFSheet sh1 = wb.getSheetAt(0);
+
+            for (Object[] each : returnValue) {
+                each[0] = new DataFormatter().formatCellValue(sh1.getRow(start).getCell(0));
+                each[1] = new DataFormatter().formatCellValue(sh1.getRow(start).getCell(1));
+                start++;
+            }
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+        }
+        return returnValue;
+    }
+
+
+    @Test(dataProvider = "getExcelData")
+    public void negativeloginTest(String login, String pass) {
+        Homepage.logining(login, pass);
+        Assert.assertEquals(Homepage.loginFailed(), "Ошибка авторизации");
+    }
+
     @AfterClass
-    public void Down() {
+    public void tearDown() {
         driver.quit();
     }
 
